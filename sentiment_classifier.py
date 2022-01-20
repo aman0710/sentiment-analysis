ef1f32c0f153dc6a2495e0691a0811d8b10877e8
Aman Kumar <amanofficial16@gmail.com>
2022-01-21 00:40:20 +0530

    added f1 score

--- sentiment_classifier.py
+++ sentiment_classifier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[222]:
+# In[2]:
 
 
 import random
@@ -40,7 +40,7 @@ class ReviewContainer:
         random.shuffle(self.reviews)
 
 
-# In[223]:
+# In[3]:
 
 
 import json
@@ -57,13 +57,13 @@ with open(file_name) as f:
 reviews[5].text
 
 
-# In[224]:
+# In[4]:
 
 
 len(reviews)
 
 
-# In[225]:
+# In[5]:
 
 
 from sklearn.model_selection import train_test_split
@@ -77,57 +77,57 @@ train_container.evenly_distribute()
 test_container.evenly_distribute()
 
 
-# In[226]:
+# In[6]:
 
 
 len(training)
 
 
-# In[227]:
+# In[7]:
 
 
 print(training[0].sentiment)
 
 
-# In[228]:
+# In[8]:
 
 
 train_x = train_container.get_text()
 train_y = train_container.get_sentiment()
 
 
-# In[229]:
+# In[9]:
 
 
 train_x[1]
 
 
-# In[230]:
+# In[10]:
 
 
 train_y[1]
 
 
-# In[231]:
+# In[11]:
 
 
 test_x = test_container.get_text()
 test_y = test_container.get_sentiment()
 
 
-# In[232]:
+# In[12]:
 
 
 test_x[0]
 
 
-# In[233]:
+# In[13]:
 
 
 test_y[0]
 
 
-# In[234]:
+# In[14]:
 
 
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -140,92 +140,92 @@ test_x_vectors = vectorizer.transform(test_x)
 print(train_x_vectors[0].toarray())
 
 
-# In[235]:
+# In[15]:
 
 
 train_x_vectors
 
 
-# In[236]:
+# In[16]:
 
 
 test_x_vectors
 
 
-# In[237]:
+# In[17]:
 
 
 test_x_vectors
 
 
-# In[238]:
+# In[18]:
 
 
 test_x_vectors[0]
 
 
-# In[239]:
+# In[19]:
 
 
 from sklearn import svm
 
 
-# In[240]:
+# In[20]:
 
 
 clf_svm = svm.SVC(kernel='linear')
 clf_svm.fit(train_x_vectors, train_y)
 
 
-# In[241]:
+# In[21]:
 
 
 test_x[0]
 
 
-# In[242]:
+# In[22]:
 
 
 test_y[0]
 
 
-# In[243]:
+# In[23]:
 
 
 print(clf_svm.predict(test_x_vectors[0]))
 
 
-# In[244]:
+# In[24]:
 
 
 user_input = "Overall an average book i would say. Did not enjoy much"
 
 
-# In[245]:
+# In[25]:
 
 
 user_input_vector = vectorizer.transform([user_input])
 
 
-# In[246]:
+# In[26]:
 
 
 print(user_input_vector)
 
 
-# In[247]:
+# In[27]:
 
 
 print(clf_svm.predict(user_input_vector))
 
 
-# In[248]:
+# In[28]:
 
 
 print(clf_svm.predict(test_x_vectors))
 
 
-# In[249]:
+# In[29]:
 
 
 from sklearn.tree import DecisionTreeClassifier
@@ -234,13 +234,13 @@ clf_dec = DecisionTreeClassifier()
 clf_dec.fit(train_x_vectors, train_y)
 
 
-# In[250]:
+# In[30]:
 
 
 print(clf_dec.predict(user_input_vector[0]))
 
 
-# In[251]:
+# In[31]:
 
 
 from sklearn.naive_bayes import GaussianNB
@@ -249,13 +249,13 @@ clf_gnb = GaussianNB()
 clf_gnb.fit(train_x_vectors.todense(), train_y)
 
 
-# In[252]:
+# In[32]:
 
 
 print(clf_gnb.predict(user_input_vector.todense()))
 
 
-# In[253]:
+# In[33]:
 
 
 from sklearn.linear_model import LogisticRegression
@@ -264,13 +264,13 @@ clf_log = LogisticRegression()
 clf_log.fit(train_x_vectors, train_y)
 
 
-# In[254]:
+# In[34]:
 
 
 print(clf_log.predict(test_x_vectors[2]))
 
 
-# In[255]:
+# In[35]:
 
 
 # Mean Accuracy
@@ -282,7 +282,7 @@ print(clf_gnb.score(test_x_vectors.todense(), test_y))
 print(clf_log.score(test_x_vectors, test_y))
 
 
-# In[257]:
+# In[36]:
 
 
 # F1 Scores
@@ -295,26 +295,26 @@ print(f1_score(test_y, clf_gnb.predict(test_x_vectors.todense()), average=None, 
 print(f1_score(test_y, clf_log.predict(test_x_vectors), average=None, labels=[Sentiment.POSITIVE, Sentiment.NEGATIVE]))
 
 
-# In[262]:
+# In[37]:
 
 
 test_y.count(Sentiment.POSITIVE)
 
 
-# In[263]:
+# In[38]:
 
 
 test_set = ['good read', 'bad book do not buy', 'horrible waste of time', 'Overall a mild book i would say. Did not enjoy much']
 new_test = vectorizer.transform(test_set)
 
 
-# In[264]:
+# In[39]:
 
 
 print(clf_log.predict(new_test))
 
 
-# In[265]:
+# In[40]:
 
 
 from sklearn.model_selection import GridSearchCV
@@ -327,13 +327,19 @@ clf = GridSearchCV(log_reg, parameters, cv=5)
 clf.fit(train_x_vectors, train_y)
 
 
-# In[266]:
+# In[41]:
 
 
 print(clf.score(test_x_vectors, test_y))
 
 
-# In[267]:
+# In[42]:
+
+
+print(f1_score(test_y, clf.predict(test_x_vectors), average=None, labels=[Sentiment.POSITIVE, Sentiment.NEGATIVE]))
+
+
+# In[43]:
 
 
 import pickle
@@ -342,14 +348,14 @@ with open('sentiment_classifier.pkl', 'wb') as f:
     pickle.dump(clf, f)
 
 
-# In[268]:
+# In[44]:
 
 
 with open('sentiment_classifier.pkl', 'rb') as f:
     loaded_clf = pickle.load(f)
 
 
-# In[269]:
+# In[45]:
 
 
 print(test_y[3])
